b393e3f2eb8ae1720629dc2f8e914225ffd9a66c
Escort27 <1176983815@qq.com>
2022-11-11 18:11:04 +0800

    tijiao

---
+++ src/main/java/com/example/team12/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.team12.service;
+
+import com.example.team12.bean.User;
+
+import java.util.List;
+
+/**
+ * @author cr
+ * &#064;date   2022/11/11
+ */
+public interface UserService {
+    List<User> query();
+}
