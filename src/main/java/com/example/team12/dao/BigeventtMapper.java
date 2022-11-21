eb82718f3cae9d51522a66ba6469586ce2e495df
illidanlily <112677815+illidanlily@users.noreply.github.com>
2022-11-22 00:14:50 +0800

    Add files via upload

--- src/main/java/com/example/team12/dao/BigeventtMapper.java
+++ src/main/java/com/example/team12/dao/BigeventtMapper.java
@@ -1,11 +1,15 @@
 package com.example.team12.dao;
 
 import com.example.team12.bean.Bigevent;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
+@Mapper
 public interface BigeventtMapper {
-    @Select("select title,link,date from bigevent order by date DESC limit 0,50")
-    List<Bigevent> query();
+    @Select("select title,link,date from bigevent order by date DESC limit #{x},10")
+    public List<Bigevent> query(@Param("x") int x);
+
 }
