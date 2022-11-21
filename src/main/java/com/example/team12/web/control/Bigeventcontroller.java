c1476e5c0333ed669abe465938dee96407ad18a2
illidanlily <112677815+illidanlily@users.noreply.github.com>
2022-11-22 00:16:27 +0800

    Add files via upload

--- src/main/java/com/example/team12/web/control/Bigeventcontroller.java
+++ src/main/java/com/example/team12/web/control/Bigeventcontroller.java
@@ -3,20 +3,28 @@ package com.example.team12.web.control;
 import com.example.team12.bean.Bigevent;
 import com.example.team12.service.Impl.BigeventmapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/bigevent")
 public class Bigeventcontroller {
-    @Autowired
-    private BigeventmapperImpl bigeventmapper;
+    @Resource
+    public BigeventmapperImpl bigeventmapper;
 
-    @GetMapping("/query")
-    public List<Bigevent> query(){
-        return bigeventmapper.query();
+    @RequestMapping(value = "/bigevent",method = RequestMethod.POST)
+    public Map<String,Object> getbigevent(@RequestParam("x") int x){
+        x=(x-1)*10;
+        bigeventmapper.bigeventselect(x);
+        HashMap<String,Object> hashMap=new HashMap<>();
+        hashMap.put("code",200);
+        hashMap.put("message","请求成功");
+        return hashMap;
     }
 }
