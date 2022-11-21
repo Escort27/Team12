b5b8e3207753a116d6d8fdd47a9affafa0b7316a
illidanlily <112677815+illidanlily@users.noreply.github.com>
2022-11-22 00:15:38 +0800

    Add files via upload

--- src/main/java/com/example/team12/service/Impl/BigeventmapperImpl.java
+++ src/main/java/com/example/team12/service/Impl/BigeventmapperImpl.java
@@ -2,19 +2,19 @@ package com.example.team12.service.Impl;
 
 import com.example.team12.bean.Bigevent;
 import com.example.team12.dao.BigeventtMapper;
-import com.example.team12.service.BigeventMapperService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @Service
-public class BigeventmapperImpl implements BigeventMapperService {
+public class BigeventmapperImpl  {
     @Resource
     private BigeventtMapper bigeventtMapper;
 
-    @Override
-    public List<Bigevent> query() {
-        return bigeventtMapper.query();
+    public void bigeventselect(int x){
+        bigeventtMapper.query(x);
     }
+
+
 }
