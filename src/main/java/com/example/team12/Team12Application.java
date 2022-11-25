c000f85a310e5574893fde7812f969f14af36267
illidanlily <112677815+illidanlily@users.noreply.github.com>
2022-11-25 13:34:11 +0800

    Add files via upload

--- src/main/java/com/example/team12/Team12Application.java
+++ src/main/java/com/example/team12/Team12Application.java
@@ -3,13 +3,14 @@ package com.example.team12;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @MapperScan("com.example.team12.dao")
 @SpringBootApplication
+@EnableScheduling
 public class Team12Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Team12Application.class, args);
 	}
-
 }
