e0c3d65c99adc251635a73714ef3a571ea634d66
illidanlily <112677815+illidanlily@users.noreply.github.com>
2022-11-25 17:12:10 +0800

    Delete src/main/java/com/example/team12 directory

--- src/main/java/com/example/team12/dao/BigeventInsert.java
+++
@@ -1,7 +0,0 @@
-package com.example.team12.dao;
-import com.example.team12.bean.Bigevent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BigeventInsert extends JpaRepository<Bigevent,String> {
-
-}
--- src/main/java/com/example/team12/service/HttpInsertService.java
+++
@@ -1,7 +0,0 @@
-package com.example.team12.service;
-
-import java.util.Map;
-
-public interface HttpInsertService {
-    int addStreamline(Map map);
-}
