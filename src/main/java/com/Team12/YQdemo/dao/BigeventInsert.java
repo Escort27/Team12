328cfe162b6cb9276faa2800070b22a8b2036f98
Dnaoatan1226 <113358740+Dnaoatan1226@users.noreply.github.com>
2022-11-25 18:46:05 +0800

    Delete src directory

--- src/main/java/com/Team12/YQdemo/dao/BigeventInsert.java
+++
@@ -1,7 +0,0 @@
-package com.team12.YQdemo.dao;
-import com.team12.YQdemo.bean.Bigevent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BigeventInsert extends JpaRepository<Bigevent,String> {
-
-}
--- src/main/java/com/Team12/YQdemo/service/HttpInsertService.java
+++
@@ -1,7 +0,0 @@
-package com.team12.YQdemo.service;
-
-import java.util.Map;
-
-public interface HttpInsertService {
-    int addStreamline(Map map);
-}
